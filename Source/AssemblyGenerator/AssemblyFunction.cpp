21aea667ee05c989c618a763ef3017203638a9c5
batburger <a_modenbach@outlook.com>
2019-03-21 12:46:35 +0100

    Abstracted the mov instruction into a function.

--- Source/AssemblyGenerator/AssemblyFunction.cpp
+++ Source/AssemblyGenerator/AssemblyFunction.cpp
@@ -6,8 +6,52 @@
 
 #include "AssemblyFunction.h"
 
+
 void ACC::AssemblyFunction::writeLine(const std::string &line) {
     code += line + "\n";
 }
 
 ACC::AssemblyFunction::AssemblyFunction(std::string sym) : symbol(std::move(sym)) {}
+
+void ACC::AssemblyFunction::mov(std::string target, std::string source, ACC::SizeDefinition size, bool ptr,
+        std::string comment) {
+    /*
+         * Cases:
+         *
+         * mov <size> <ptr?> [<register>], <immediate>
+         * mov <size> <ptr?> [<register>], <register>
+         * e.g.:
+         * mov dword [rsp], 0x0a30
+         * mov dword ptr [rax], 0x00
+         * */
+
+        std::string ptrStr = ptr ? ("ptr ") : ("");
+        writeLine("mov " + sizeToString(size) + " " + ptrStr + "[" + target + "], " + source + " ; " + comment);
+
+}
+
+void ACC::AssemblyFunction::mov(std::string target, std::string source, std::string comment) {
+        /*
+         * Cases:
+         *
+         * mov <register>, <immediate>
+         * mov <register>, <register>
+         * e.g.:
+         * mov eax, 10
+         * */
+
+        writeLine("mov " + target + ", " + source + " ; " + comment);
+}
+
+std::string ACC::sizeToString(ACC::SizeDefinition size) {
+    switch (size){
+        case SizeDefinition::dowrd:
+            return "dword";
+        case SizeDefinition::qword:
+            return "qword";
+        case SizeDefinition::word:
+            return "word";
+        case SizeDefinition::byte:
+            return "byte";
+    }
+}
--- Source/AssemblyGenerator/AssemblyFunction.h
+++ Source/AssemblyGenerator/AssemblyFunction.h
@@ -8,6 +8,16 @@
 #include <string>
 
 namespace ACC {
+
+    enum class SizeDefinition{
+        dowrd,
+        qword,
+        word,
+        byte
+    };
+
+    std::string sizeToString(SizeDefinition size);
+
     class AssemblyFunction {
     public:
         std::string code;
@@ -18,6 +28,13 @@ namespace ACC {
         explicit AssemblyFunction(std::string sym);
 
         void writeLine(const std::string & line);
+
+        void mov(std::string target, std::string source, std::string comment = "");
+        void mov(std::string target, std::string source, SizeDefinition size, bool ptr = false,
+                 std::string comment = "");
+
+
+
 
     };
 }
