7e7ddb5546103af87fe470947f7874aa1782672b
batburger <a_modenbach@outlook.com>
2019-03-24 17:22:09 +0100

    Added IR generation for prints with literals and exits with variables.

--- Source/IntermediateCodeGenerator/TokenGenerator/ExitTokenGenerator.cpp
+++ Source/IntermediateCodeGenerator/TokenGenerator/ExitTokenGenerator.cpp
@@ -5,8 +5,17 @@ ACC::ExitTokenGenerator::ExitTokenGenerator(ACC::ASTNode *node) : Expr(node) {
 }
 
 ACC::Dependency ACC::ExitTokenGenerator::generate(ACC::Code &code) {
-    auto exitCode = (temporary)std::stoi(node->children[0]->str);
-    auto op = new Operator(OperatorId::EXIT, exitCode, 0, 0);
-    code.pushOp(op);
+    if(node->children[0]->op == AstOperator::LITERAL){
+        auto exitCode = (temporary)std::stoi(node->children[0]->str);
+        auto op = new Operator(OperatorId::IEXIT, exitCode, 0, 0);
+        code.pushOp(op);
+
+    } else{ // its a exit with an variable e.g.: " var a = 10; exit a; "
+        Dependency& var = code.getVarSymbol(node->children[0]->str);
+        auto op = new Operator(OperatorId::EXIT, var.temp, 0, 0);
+        op->opLhs = var.op;
+        var.op->opResult = op;
+        code.pushOp(op);
+    }
     return {};
 }
--- Source/IntermediateCodeGenerator/TokenGenerator/PrintTokenGenerator.cpp
+++ Source/IntermediateCodeGenerator/TokenGenerator/PrintTokenGenerator.cpp
@@ -5,10 +5,16 @@ ACC::PrintTokenGenerator::PrintTokenGenerator(ACC::ASTNode *node) : Expr(node) {
 }
 
 ACC::Dependency ACC::PrintTokenGenerator::generate(ACC::Code &code) {
-    Dependency& var = code.getVarSymbol(node->children[0]->str);
-    auto op = new Operator(OperatorId::PRINT, var.temp, 0, 0);
-    op->opLhs = var.op;
-    var.op->opResult = op;
-    code.pushOp(op);
+
+    if(node->children[0]->op == AstOperator::ID){
+        Dependency& var = code.getVarSymbol(node->children[0]->str);
+        auto op = new Operator(OperatorId::PRINT, var.temp, 0, 0);
+        op->opLhs = var.op;
+        var.op->opResult = op;
+        code.pushOp(op);
+    }else{ // its a print with a literal e.g.: "print 10;"
+        auto op = new Operator(OperatorId::IPRINT, (temporary)std::stoi(node->children[0]->str), 0, 0);
+        code.pushOp(op);
+    }
     return {};
 }
