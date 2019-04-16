8f8d02e0a5a4c1de8eda502257e9f8f1666b3179
batburger <a_modenbach@outlook.com>
2019-04-16 18:26:06 +0200

    Added movements required for function calls.

--- Source/AssemblyGenerator/DataStructureMovement.cpp
+++ Source/AssemblyGenerator/DataStructureMovement.cpp
@@ -5,12 +5,17 @@
 #include "DataStructureMovement.h"
 #include <utils.h>
 
-std::string ACC::Movs::c2st(std::vector<Location> constants){
+std::string ACC::Movs::c2st(Location constant){
+    if(constant.accessMethod != AccessMethod::CONSTANT)
+        throw std::runtime_error("Called data structure movement method for movement of constants with `Location` objects"
+                                 "not containing constants.");
+
+
+    std::string value = constant.constant;
     std::string str = "mov dword [rsp], 0x";
     size_t count = 0;
 
-    for (size_t i = 0; i < constants.size(); i++) {
-        if (constants[i].accessMethod == AccessMethod::CONSTANT) {
+    for (size_t i = 0; i < value.size(); i++) {
             size_t offset = str.size();
             if (count == 4) {
                 str += "\nmov dword [rsp + " + std::to_string(i) + "], 0x";
@@ -18,9 +23,76 @@ std::string ACC::Movs::c2st(std::vector<Location> constants){
                 count = 0;
             }
 
-            str.insert(offset, toHex((unsigned)constants.at((unsigned)i).constant));
+            str.insert(offset, toHex((unsigned)value.at(i)));
             count++;
-        }
     }
     return str;
 }
+
+std::string ACC::Movs::bp2st(ACC::Location where, Stack<size_t> &structure) {
+    if(where.accessMethod != AccessMethod::SBP_OFFSET)
+        throw std::runtime_error("Called data structure movement method for movement of data stored as an offset from "
+                                 "the stack base pointer with a `Location` object not containing such an structure.");
+
+    std::string out;
+    auto offset = where.offsetInfo;
+    size_t size = structure.pop();
+
+    for(size_t i = 0; i < size; i++){
+        out += "\nmov byte al, [rbp + "+std::to_string(offset)+"]";
+        out += "\nmov byte [rsp + "+std::to_string(i)+"], al";
+        offset += 1;
+    }
+    return out;
+}
+
+std::string ACC::Movs::c2bp(Location constant, ACC::Location where) {
+    auto bpOffset = where.offsetInfo;
+    auto sign = std::string(bpOffset >= 0 ? ("+") : ("-"));
+    std::string str = "mov dword [rbp "+sign+" "+std::to_string(bpOffset)+"], 0x";
+    size_t count = 0;
+    std::string value = constant.constant;
+
+    for (size_t i = 0; i < value.size(); i++) {
+        size_t offset = str.size();
+        if (count == 4) {
+            str += "\nmov dword [rbp "+sign+" "+std::to_string(bpOffset + i)+"], 0x";
+            offset = str.size();
+            count = 0;
+        }
+
+        str.insert(offset, toHex((unsigned)value.at(i)));
+        count++;
+    }
+
+    return str;
+}
+
+std::string ACC::Movs::r2bp(ACC::Location where, ACC::Location reg) {
+    // TODO: Utterly ignores sizes.
+
+    return "mov [rbp + " + std::to_string(where.offsetInfo) + "], "+registerToString(reg.regInfo);
+}
+
+std::string ACC::Movs::c2so(ACC::Location constant, ACC::Location where) {
+    auto spOffset = where.offsetInfo;
+    auto sign = std::string(spOffset >= 0 ? ("+") : ("-"));
+
+    std::string str = "mov dword [rsp " + sign + " " + std::to_string(spOffset)+"], 0x";
+    size_t count = 0;
+    std::string value = constant.constant;
+
+    for (size_t i = 0; i < value.size(); i++) {
+        size_t offset = str.size();
+        if (count == 4) {
+            str += "\nmov dword [rsp " + sign +" "+std::to_string(spOffset + i)+"], 0x";
+            offset = str.size();
+            count = 0;
+        }
+
+        str.insert(offset, toHex((unsigned)value.at(i)));
+        count++;
+    }
+    return str;
+}
+
--- Source/AssemblyGenerator/DataStructureMovement.h
+++ Source/AssemblyGenerator/DataStructureMovement.h
@@ -2,13 +2,26 @@
 
 #include <string>
 #include <vector>
+#include <Stack.h>
 #include <AssemblyGenerator/Location.h>
 
-namespace ACC{
-    namespace Movs{
+namespace ACC {
+    namespace Movs {
 
-        /*! Moves a vector of constants onto the top of the stack.*/
-        std::string c2st(std::vector<Location> constants);
+        /*! Creates a string which contains x86_64 assembler to have constants (given as vector \p constants) on top of
+         * the stack.*/
+        std::string c2st(Location value);
 
+        /*! Creates a string which contains x86_64 assembler to copy data stored at an offset from the base pointer on top
+         * op the stack.*/
+        std::string bp2st(Location where, Stack<size_t> &structure);
+
+        /*! Creates a string which contains x86_64 assembler to have constants (given as vector \p constants) at some
+         * offset from the base pointer.*/
+        std::string c2bp(Location constant, Location where);
+
+        std::string r2bp(Location where, Location reg);
+
+        std::string c2so(Location constant, Location where);
     }
 }
