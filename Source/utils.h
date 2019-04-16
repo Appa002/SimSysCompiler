407584c439d1f6803f0cce043d999a059191e143
batburger <a_modenbach@outlook.com>
2019-04-16 15:05:16 +0200

    Added docs.

--- Source/utils.h
+++ Source/utils.h
@@ -11,8 +11,23 @@
 #include <sstream>
 #include <ios>
 
+/*! Expresses the parameter \p value as an x86 worthy hex value
+ * (The return value doesn't contain the `0x` or `h` suffix/prefix).
+ * Every byte of the parameter \p value is expressed with to hex digits (e.g.: (int) 0 becomes (hex) 00)
+ * @param value The value to convert.
+ * @return String that represents the parameter \p value.*/
 std::string toHex (int value);
 
+/*! Splits up an object (\p itrAble) of arbitrary type `T` at every 4th element; calls function f with a vector containing
+ * the 4 elements from the last split.
+ * If the dataset contains elements at the end of the dataset which wouldn't naturally fall into a packet (e.g.: the
+ * `5` in `1 2 3 4 5`), then this function will call \p f for a final time with the last dangling packet.
+ * @param itrAble The object, of type `T`, which contains the data to split.
+ * @param f A function which gets called for every split packet, as-well-as for the last dangling packet. It must take a
+ * `std::vector<` *ELM* `>` as argument; it will contain the packet.
+ * @tparam T The type of the \p itrAble object. It must implement: `operator[]` and `.size()` in a meaningful manner.
+ * @tparam ELM The type of one element of an object of type `T`. (e.g.: `ELM` would be `char` if `T` is `std::string`).
+ * */
 template <class T, class ELM>
 void dWordAlignT(T itrAble, std::function<void(std::vector<ELM>)> f){
     std::vector<ELM> packet;
