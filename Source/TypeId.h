cc1046f4ccd68dfb22053ce38a87300e4a42d0be
batburger <a_modenbach@outlook.com>
2019-05-11 16:44:20 +0200

    TypeIDs now keep track of type size.

--- Source/TypeId.h
+++ Source/TypeId.h
@@ -8,20 +8,22 @@
 namespace ACC {
     struct TypeId {
         TypeId() = default;
-        TypeId(size_t id) : id(id) {}
+        TypeId(size_t id, size_t size) : id(id), size(size){}
 
-        size_t toInt() {return id;}
+        size_t getId() const {return id;}
+        size_t getSize() const {return size;}
 
         friend inline bool operator==(TypeId const &lhs, TypeId const &rhs) {
-            return lhs.id == rhs.id;
+            return lhs.id == rhs.id && lhs.size == rhs.size;
         }
 
         friend inline bool operator!=(TypeId const &lhs, TypeId const &rhs) {
-            return lhs.id != rhs.id;
+            return !(lhs == rhs);
         }
 
     private:
         size_t id = 0;
+        size_t size = 0;
     };
 }
 
