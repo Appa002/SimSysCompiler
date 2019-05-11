94e0f417fdb04b7c31cd1640cd6a0da90bf5bbd6
batburger <a_modenbach@outlook.com>
2019-05-11 16:48:10 +0200

    Now uses proper types.

--- Source/Assembly/TokenGenerator/ExitTokenGenerator.cpp
+++ Source/Assembly/TokenGenerator/ExitTokenGenerator.cpp
@@ -1,3 +1,4 @@
+#include <builtinTypes.h>
 #include "ExitTokenGenerator.h"
 
 ACC::ExitTokenGenerator::ExitTokenGenerator(ACC::ASTNode *node) : Expr(node) {
@@ -7,6 +8,9 @@ ACC::ExitTokenGenerator::ExitTokenGenerator(ACC::ASTNode *node) : Expr(node) {
 ACC::Structure ACC::ExitTokenGenerator::generate(ACC::Code &code) {
     auto& fn = code.getFnSymbol();
     auto expr = node->children[0]->asExpr()->generate(code);
+
+    if(expr.type != BuiltIns::numType)
+        throw std::runtime_error("Exit needs to be called with a number as argument.");
 
     fn.writeLine(expr.copyToRegister("rdi", code));
     fn.writeLine("mov rax, 60");
