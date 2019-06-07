2c2ad113ec213a4448a79595ffb3a069a34c2583
batburger <a_modenbach@outlook.com>
2019-06-07 17:59:26 +0200

    Implemented using new Architecture.

--- Source/AbstractSyntaxTree/ASTNodes/CallNode.cpp
+++ Source/AbstractSyntaxTree/ASTNodes/CallNode.cpp
@@ -1,8 +1,29 @@
 #include <utility>
+#include <Structure/Structures/NumLValueStructure.h>
+#include <Structure/Structures/NumRValueStructure.h>
+#include <builtinTypes.h>
 
 #include "CallNode.h"
 
 std::shared_ptr<ACC::Structure> ACC::CallNode::generate(ACC::Code &code) {
+    auto& fn = code.getFnSymbol();
+    size_t totalRspSubtracted = 0;
+
+    for(size_t i = children.size() - 1; i >= 1; i--){
+        auto value = children[i]->generate(code);
+        fn.writeLine("sub rsp, " + std::to_string(value->type.getSize()));
+        totalRspSubtracted += value->type.getSize();
+
+        if(value->type == BuiltIns::numType)
+            value->operatorCopy(std::make_shared<NumLValueStructure>("rsp"), code);
+    }
+
+    auto name = children[0]->data.asT<std::string>();
+    fn.writeLine("call " + name);
+    fn.writeLine("add rsp, " + std::to_string(totalRspSubtracted));
+    return std::make_shared<NumRValueStructure>(Register::rA);
+
+
   /*  auto& fn = code.getFnSymbol();
     size_t totalRspSubtracted = 0;
     for(size_t i = node->children.size() - 1; i >= 1; i--){
