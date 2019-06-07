bf0bfc247acf9cf06df935f2b16a821ee36b2709
batburger <a_modenbach@outlook.com>
2019-06-07 15:52:39 +0200

    Implemented using new system.

--- Source/AbstractSyntaxTree/ASTNodes/SeqNode.cpp
+++ Source/AbstractSyntaxTree/ASTNodes/SeqNode.cpp
@@ -3,16 +3,13 @@
 #include "SeqNode.h"
 
 std::shared_ptr<ACC::Structure> ACC::SeqNode::generate(ACC::Code &code) {
-    /*if(node->children.size() == 2)
-        node->children[1]->asExpr()->generate(code);
-    node->children[0]->asExpr()->generate(code);
-    return {};*/
+    if(children.size() == 2)
+        children[1]->generate(code);
+    children[0]->generate(code);
     return nullptr;
 }
 
-ACC::SeqNode::SeqNode(ACC::AstOperator op, std::vector<ACC::ASTNode *> children) : ASTNode(op,
-                                                                                                               std::move(
-                                                                                                                       children)) {
+ACC::SeqNode::SeqNode(ACC::AstOperator op, std::vector<ACC::ASTNode *> children) : ASTNode(op,std::move(children)) {
 
 }
 
