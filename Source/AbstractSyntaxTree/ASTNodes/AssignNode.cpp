a177a9dbc051d227003f7b6009f032e03724f4f9
batburger <a_modenbach@outlook.com>
2019-06-07 20:35:52 +0200

    Added support for literal token.

--- Source/AbstractSyntaxTree/ASTNodes/AssignNode.cpp
+++ Source/AbstractSyntaxTree/ASTNodes/AssignNode.cpp
@@ -3,7 +3,9 @@
 
 #include "AssignNode.h"
 #include <memory>
-
+#include <Structure/Structures/ElementaryLValueStructure.h>
+#include <General/builtinTypes.h>
+#include <Structure/Structures/PtrLValueStructure.h>
 
 
 std::shared_ptr<ACC::Structure> ACC::AssignNode::generate(ACC::Code &code) {
@@ -15,7 +17,13 @@ std::shared_ptr<ACC::Structure> ACC::AssignNode::generate(ACC::Code &code) {
 
     fn.curBpOffset += expr->type.getSize();
 
-    std::shared_ptr<NumLValueStructure> address = std::make_shared<NumLValueStructure>("rbp - " + std::to_string(fn.curBpOffset));
+    std::shared_ptr<Structure> address;
+
+    if(type == BuiltIns::numType)
+        address = std::make_shared<NumLValueStructure>("rbp - " + std::to_string(fn.curBpOffset));
+    else if (type == BuiltIns::ptrCharType)
+        address = std::make_shared<PtrLValueStructure>("rbp - " + std::to_string(fn.curBpOffset));
+
     expr->operatorCopy(address, code);
 
     code.emplaceVarSymbol(id, address);
