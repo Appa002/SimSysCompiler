5c73f21b1b6be35cd013550acff7ae918c25f824
batburger <a_modenbach@outlook.com>
2019-06-07 18:50:44 +0200

    Implemented for loop using new Architecture.

--- Source/AbstractSyntaxTree/ASTNodes/ForNode.cpp
+++ Source/AbstractSyntaxTree/ASTNodes/ForNode.cpp
@@ -18,8 +18,34 @@ std::shared_ptr<ACC::Structure> ACC::ForNode::generate(ACC::Code &code) {
      *      ...block
      * ...rest
      * */
-    /*
 
+
+    auto& fn = code.getFnSymbol();
+    auto top = code.getUUID();
+    auto rest = code.getUUID();
+
+    fn.writeLine("." + top + ":");
+
+    auto index = children[0]->generate(code);
+    auto limit = children[1]->generate(code);
+    index->operatorForDone(limit, code);
+
+    // At this point rflags has been set according to the comparison of index and limit
+
+    fn.writeLine("jne ." + rest);
+    code.pushScope();
+    children[2]->generate(code);
+    code.popScope();
+
+    // Iterating
+    index->operatorForNext(code);
+
+    // Looping back up / seting up rest..
+    fn.writeLine("jmp ." + top);
+    fn.writeLine("."+rest+":");
+    return nullptr;
+
+    /*
     auto& fn = code.getFnSymbol();
     auto top = code.getUUID();
     auto rest = code.getUUID();
