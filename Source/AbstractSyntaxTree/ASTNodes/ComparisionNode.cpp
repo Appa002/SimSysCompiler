2167cb5391b2a5379ed226ad23ae2214dcf37ae9
batburger <a_modenbach@outlook.com>
2019-06-07 21:11:43 +0200

    Implemented using new Architecture.

--- Source/AbstractSyntaxTree/ASTNodes/ComparisionNode.cpp
+++ Source/AbstractSyntaxTree/ASTNodes/ComparisionNode.cpp
@@ -7,6 +7,25 @@
 
 
 std::shared_ptr<ACC::Structure> ACC::ComparisionNode::generate(ACC::Code &code) {
+    auto lhs = children[0]->generate(code);
+    auto rhs = children[1]->generate(code);
+    auto& fn = code.getFnSymbol();
+
+    switch (cmpType){
+        case ComparisionType::EQ:
+            return lhs->operatorEqual(rhs, code);
+        case ComparisionType::LT:
+            return lhs->operatorLess(rhs, code);
+        case ComparisionType::GT:
+            return lhs->operatorGreater(rhs, code);
+        case ComparisionType::NEQ:
+            return lhs->operatorNotEqual(rhs, code);
+        case ComparisionType::LET:
+            return lhs->operatorLessEqual(rhs, code);
+        case ComparisionType::GET:
+            return lhs->operatorGreaterEqual(rhs, code);
+    }
+
 /*    auto lhs = node->children[0]->asExpr()->generate(code);
     auto rhs = node->children[1]->asExpr()->generate(code);
     auto& fn = code.getFnSymbol();
