ecdb1a924e911fb181d63d562f9e91e50d862c3b
batburger <a_modenbach@outlook.com>
2019-06-07 20:32:28 +0200

    Implemented using new Architecture.

--- Source/AbstractSyntaxTree/ASTNodes/ReturnNode.cpp
+++ Source/AbstractSyntaxTree/ASTNodes/ReturnNode.cpp
@@ -1,9 +1,24 @@
 #include <utility>
+#include <Structure/Structures/ElementaryStructure.h>
 
 #include "ReturnNode.h"
 
 std::shared_ptr<ACC::Structure> ACC::ReturnNode::generate(ACC::Code &code) {
-  /*  auto returnValue = node->children[0]->asExpr()->generate(code);
+    auto returnValue = children[0]->generate(code);
+    auto &fn = code.getFnSymbol();
+
+    auto* asElem = dynamic_cast<ElementaryStructure*>(returnValue.get());
+    if(asElem)
+        asElem->loadToRegister(Register::rA, code);
+    else
+        ; //TODO: Figure out what to do for none elementary types....
+
+    fn.writeLine("add rsp, " + std::to_string(fn.curBpOffset));
+    fn.writeLine("pop rbp");
+    fn.writeLine("ret");
+
+    return nullptr;
+    /*  auto returnValue = node->children[0]->asExpr()->generate(code);
     auto& fn = code.getFnSymbol();
 
     fn.writeLine("add rsp, " + std::to_string(fn.curBpOffset));
@@ -18,6 +33,7 @@ std::shared_ptr<ACC::Structure> ACC::ReturnNode::generate(ACC::Code &code) {
     return nullptr;
 }
 
-ACC::ReturnNode::ReturnNode(ACC::AstOperator op, std::vector<ACC::ASTNode *> children) : ASTNode(op, std::move(children)) {
+ACC::ReturnNode::ReturnNode(ACC::AstOperator op, std::vector<ACC::ASTNode *> children) : ASTNode(op,
+                                                                                                 std::move(children)) {
 
 }
