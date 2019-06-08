275c2b57593d0b938126df1fdeaf7fe9965284b8
batburger <a_modenbach@outlook.com>
2019-06-09 00:19:16 +0200

    Changed so it uses the `leave` instruction.

--- Source/AbstractSyntaxTree/ASTNodes/ReturnNode.cpp
+++ Source/AbstractSyntaxTree/ASTNodes/ReturnNode.cpp
@@ -15,10 +15,15 @@ std::shared_ptr<ACC::Structure> ACC::ReturnNode::generate(ACC::Code &code) {
 
     returnValue->cleanUp(code);
 
-    fn.writeLine("add rsp, " + std::to_string(fn.curBpOffset));
+   /* fn.writeLine("add rsp, " + std::to_string(fn.curBpOffset));
     fn.writeLine("pop rbp");
-    fn.writeLine("ret");
+    fn.writeLine("ret");*/
 
+   /* TODO: Figure out how `leave` magically cleans up the stack when using buffers with variable sizes
+    * TODO:, even though such doesn't seem to be immediately doable using the method above.*/
+
+    fn.writeLine("leave");
+    fn.writeLine("ret");
     return nullptr;
 }
 
