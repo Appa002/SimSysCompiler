a895b08c5c6b4ccde188ce1363824faa01698522
batburger <a_modenbach@outlook.com>
2019-06-08 16:37:57 +0200

    Initial Commit.

---
+++ Source/Structure/Structures/Bool/BoolStructure.cpp
@@ -0,0 +1,108 @@
+//
+// Created by a_mod on 08.06.2019.
+//
+
+#include <General/builtinTypes.h>
+#include "BoolStructure.h"
+#include "BoolRValueStructure.h"
+#include <Assembly/Code.h>
+
+std::shared_ptr<ACC::Structure>
+ACC::BoolStructure::operatorEqual(std::shared_ptr<ACC::Structure> other, ACC::Code &code) {
+    auto& fn = code.getFnSymbol();
+    Register lhs = code.getFreeRegister();
+    Register rhs = code.getFreeRegister();
+
+    this->loadToRegister(lhs, code);
+    dynamic_cast<BoolStructure*>(other.get())->loadToRegister(rhs, code);
+
+    fn.writeLine("cmp " + registerToString(1, lhs) + ", " + registerToString(1, rhs));
+    fn.writeLine("sete " + registerToString(1, lhs));
+
+    code.freeRegister(rhs);
+    return std::make_shared<BoolRValueStructure>(lhs);
+}
+
+std::shared_ptr<ACC::Structure>
+ACC::BoolStructure::operatorNotEqual(std::shared_ptr<ACC::Structure> other, ACC::Code &code) {
+    auto& fn = code.getFnSymbol();
+    Register lhs = code.getFreeRegister();
+    Register rhs = code.getFreeRegister();
+
+    this->loadToRegister(lhs, code);
+    dynamic_cast<BoolStructure*>(other.get())->loadToRegister(rhs, code);
+
+    fn.writeLine("cmp " + registerToString(1, lhs) + ", " + registerToString(1, rhs));
+    fn.writeLine("setne " + registerToString(1, lhs));
+
+    code.freeRegister(rhs);
+    return std::make_shared<BoolRValueStructure>(lhs);
+}
+
+std::shared_ptr<ACC::Structure>
+ACC::BoolStructure::operatorLess(std::shared_ptr<ACC::Structure> other, ACC::Code &code) {
+    auto& fn = code.getFnSymbol();
+    Register lhs = code.getFreeRegister();
+    Register rhs = code.getFreeRegister();
+
+    this->loadToRegister(lhs, code);
+    dynamic_cast<BoolStructure*>(other.get())->loadToRegister(rhs, code);
+
+    fn.writeLine("cmp " + registerToString(1, lhs) + ", " + registerToString(1, rhs));
+    fn.writeLine("setl " + registerToString(1, lhs));
+
+    code.freeRegister(rhs);
+    return std::make_shared<BoolRValueStructure>(lhs);
+}
+
+std::shared_ptr<ACC::Structure>
+ACC::BoolStructure::operatorGreater(std::shared_ptr<ACC::Structure> other, ACC::Code &code) {
+    auto& fn = code.getFnSymbol();
+    Register lhs = code.getFreeRegister();
+    Register rhs = code.getFreeRegister();
+
+    this->loadToRegister(lhs, code);
+    dynamic_cast<BoolStructure*>(other.get())->loadToRegister(rhs, code);
+
+    fn.writeLine("cmp " + registerToString(1, lhs) + ", " + registerToString(1, rhs));
+    fn.writeLine("setg " + registerToString(1, lhs));
+
+    code.freeRegister(rhs);
+    return std::make_shared<BoolRValueStructure>(lhs);
+}
+
+std::shared_ptr<ACC::Structure>
+ACC::BoolStructure::operatorLessEqual(std::shared_ptr<ACC::Structure> other, ACC::Code &code) {
+    auto& fn = code.getFnSymbol();
+    Register lhs = code.getFreeRegister();
+    Register rhs = code.getFreeRegister();
+
+    this->loadToRegister(lhs, code);
+    dynamic_cast<BoolStructure*>(other.get())->loadToRegister(rhs, code);
+
+    fn.writeLine("cmp " + registerToString(1, lhs) + ", " + registerToString(1, rhs));
+    fn.writeLine("setle " + registerToString(1, lhs));
+
+    code.freeRegister(rhs);
+    return std::make_shared<BoolRValueStructure>(lhs);
+}
+
+std::shared_ptr<ACC::Structure>
+ACC::BoolStructure::operatorGreaterEqual(std::shared_ptr<ACC::Structure> other, ACC::Code &code) {
+    auto& fn = code.getFnSymbol();
+    Register lhs = code.getFreeRegister();
+    Register rhs = code.getFreeRegister();
+
+    this->loadToRegister(lhs, code);
+    dynamic_cast<BoolStructure*>(other.get())->loadToRegister(rhs, code);
+
+    fn.writeLine("cmp " + registerToString(1, lhs) + ", " + registerToString(1, rhs));
+    fn.writeLine("setge " + registerToString(1, lhs));
+
+    code.freeRegister(rhs);
+    return std::make_shared<BoolRValueStructure>(lhs);
+}
+
+ACC::BoolStructure::BoolStructure(ACC::ValueCategory v) : ElementaryStructure(v, Type(BuiltIns::boolType)){
+
+}
---
+++ Source/Structure/Structures/Bool/BoolStructure.h
@@ -0,0 +1,24 @@
+//
+// Created by a_mod on 08.06.2019.
+//
+
+#ifndef SIMSYSCOMPILER_BOOLSTRUCTURE_H
+#define SIMSYSCOMPILER_BOOLSTRUCTURE_H
+
+#include <Structure/Structures/ElementaryStructure.h>
+
+namespace ACC {
+    class BoolStructure : public ElementaryStructure{
+    public:
+        explicit BoolStructure(ValueCategory v);
+
+        std::shared_ptr<Structure> operatorEqual(std::shared_ptr<Structure> other, Code &code) override;
+        std::shared_ptr<Structure> operatorNotEqual(std::shared_ptr<Structure> other, Code &code) override;
+        std::shared_ptr<Structure> operatorLess(std::shared_ptr<Structure> other, Code &code) override;
+        std::shared_ptr<Structure> operatorGreater(std::shared_ptr<Structure> other, Code &code) override;
+        std::shared_ptr<Structure> operatorLessEqual(std::shared_ptr<Structure> other, Code &code) override;
+        std::shared_ptr<Structure> operatorGreaterEqual(std::shared_ptr<Structure> other, Code &code) override;
+    };
+}
+
+#endif //SIMSYSCOMPILER_BOOLSTRUCTURE_H
